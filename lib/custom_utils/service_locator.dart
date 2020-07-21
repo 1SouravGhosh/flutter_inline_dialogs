2caa7bd0be20fc825873ac8cbdb8f4b4a7fbf578
Sourav Ghosh <1SouravGhosh@noreply.github.com>
2020-07-21 14:00:42 +0530

    upgraded dependencies.

--- lib/custom_utils/service_locator.dart
+++ lib/custom_utils/service_locator.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:inline_dialogs/dialogs/service.dart';
 
-GetIt locator = GetIt();
+GetIt locator = GetIt.instance;
 
 void dialogSetupLocator() {
   locator.registerSingleton(DialogService());
